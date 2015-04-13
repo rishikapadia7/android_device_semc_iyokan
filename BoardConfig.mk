069ade46bf07e1573f20715be5721dc4a316771a
rishikapadia7 <rishikapadia7>
2015-04-12 21:43:47 -0400

    Added reference to msm7x30-common board config.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+-include device/semc/msm7x30-common/BoardConfigCommon.mk
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/iyokan/BoardConfigVendor.mk
 
